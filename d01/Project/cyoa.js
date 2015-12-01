e311cc658cfbf048833a1b82d66426560d6c68e5
Edward Yu <eboxyz@gmail.com>
2015-12-01 13:09:35 -0800

    finished all text, need to add gameover events now

--- d01/Project/cyoa.js
+++ d01/Project/cyoa.js
@@ -76,6 +76,30 @@ $(document).ready(function(){
       $('.textbox').html("That gold is actually a new genus of poisonous fungi, that exclusively grows on chocolate mousse cakes.");
       $('#option1').html("I even gave you the right choice in the last one. Are you paying attention?");
       $('#option2').html("You need to fuel your thirst."); //gameover
+    } else if(count ==15){
+      $('.textbox').html("You can choose anything you want! Wow! But you can choose only one thing of what you want, and you have to eat a whole plate of it. You also only have two options. What a surprise.");
+      $('#option1').html("Beets");
+      $('#option2').html("Chopped Black Olives");
+    } else if(count ==16){
+      $('.textbox').html("Someone likes their Beets. Too bad Dr. Dre's suing you for copyright infringement. Sucks to suck.");
+      $('#option1').html("Bears, Beets.")
+      $('#option2').html("Battlestar Galactica.") //gameover
+    } else if(count ==17){
+      $('.textbox').html("SpongeBob: 'Looks like you're a pretty good chef. I am too.'")
+      $('#option1').html("something something accept challenge");
+      $('#option2').html("if you don't ahve a spatula, you should've picked it up earlier lol...");//there sould be a gameover alert here if you didn't pick the spatula
+    } else if(count ==18){
+      $('.textbox').html("SpongeBob declares the challenge is to cook the best Krabby Patty. What's your first move?");
+      $('#option1').html("Grill Meat");
+      $('#option2').html("Chop Veggies");
+    } else if(count==19){
+      $('.textbox').html("You're really grilling the meat first? Real plebian of you. However, it is the proper way to make a gourmet Krabby Patty. What's next, Gordon Ramsey?");
+      $('#option1').html("Toast Buns");
+      $('#option2').html("Serve as is");
+    } else if(count==20){
+      $('.textbox').html("Imbecile! The judges are all on the Atkins diet, and deathly afraid of ingesting anytype of carbohydrates.");
+      $('#option1').html("These buns are from your local grocer aren't they.")
+      $('#option2').html("The esteemed judges only eat farm to table, gluten free products.") //gameover
     }
   });
 
@@ -133,6 +157,30 @@ $(document).ready(function(){
       $('.textbox').html("You decide to go to the Krusty Krab, where dreams come true. You get the option to satisfy your hunger or your thirst again.");
       $('#option1').html("Do you choose the free salad bar?");
       $('#option2').html("Do you choose the $5.00 small 2oz drink?");
+    }else if (count ==15){
+      $('.textbox').html("You died.");
+      $('#option1').html("These can't all be witty.");
+      $('#option2').html("But really, this one wasn't too hard.") //gameover
+    } else if (count==16){
+      $('.textbox').html("Them olives up to something. Justin Timberlake hands you a bottle of Olive Juice, and whispers 'Olive Juice...' sensually in your ear. He whisks you away, taking you to a local park. Spongebob approaches you.");
+      $('#option1').html("Do you talk to him?");
+      $('#option2').html("You could just ignore him too. If you wanna be a mean guy.");
+    } else if (count==17){
+      $('.textbox').html("Neptune instantly smites you.");
+      $('#option1').html("You're a jerk you know that?");
+      $('#option2').html("Be a nice guy for once."); //gameover
+    } else if(count==19){
+      $('.textbox').html("Who chops the vegetables first? What are you some kind of culinary prodigy?");
+      $('#option1').html("That'll show you.");
+      $('#option2').html("You were doomed to fail as soon as you pressed 'Start Game'.") //gameover
+    } else if(count ==20){
+      $('.textbox').html("You....You're incredible! Your skill as a chef has no peer. We are truly humbled being able to eat a hamburger so divine. We have no choice but to annoint you as head chef of Animal LA, and Son of a Gun.")
+      $('#option1').html("You've done it!");
+      $('#option2').html("Although, Spongebob is off to the side crying.");
+    } else if(count ==21){
+      $('.textbox').html("Your ascendance to the top of the culinary world has brought you massive acclaim, and even greater influence than you previously held as a figurehead of rap and fashion culture. With this influence, you are able to withstand Kim Kardashian's revenge plots, and you happily move to England with your new Bae, Susan Boyle.")
+      $('#option1').html("Congratulations.");
+      $('#option2').html("You win, I guess.");
     }
   });
 
