50fddd9a90cfdfbfeb63bcab0910bdaa3c26c772
Edward Yu <eboxyz@gmail.com>
2015-11-30 16:26:13 -0800

    changing innerhtml

--- d01/Project/cyoa.js
+++ d01/Project/cyoa.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
     $(".game").show();
   })
 
-
+  $('#option1 , #option2').click(function(){
+    var clicks = $(this).data('clicks');
+    if (clicks == 1){
+    $('#option1').html('Kanye just died...');
+    $('#option2').html('Kanye lives!')
+  } else{
+    $('#option1').html('How did Kanye come back??!?!?');
+    $('#option2').html("He's dead, okay? He's really dead.")
+  }
+    $(this).data("clicks",!clicks);
+  });
 
 
 })
