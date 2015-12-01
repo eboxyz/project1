840096c7f32085dd9d7cae200ad8e917df0477bd
Edward Yu <eboxyz@gmail.com>
2015-12-01 11:02:48 -0800

    got up to the part with the genie

--- d01/Project/cyoa.js
+++ d01/Project/cyoa.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $("#startbutton").click(function(){
     $(".start").hide();
     $(".game").show();
-  })
+  });
 
   // $('#option1 , #option2').click(function(){
   //   var clicks = $(this).data('clicks');
@@ -32,6 +32,26 @@ $(document).ready(function(){
        $('.textbox').html("The plane has crashed. They actually forgot to fuel the plane. Sucks to suck.");
        $('#option1').html("You can't play anymore, Kanye's dead...");
        $('#option2').html("Nice try, I guess.");
+    } else if(count == 4){
+      $('.textbox').html("Cool, you made it to Jay's house. What do you want to do?")
+      $('#option1').html("Talk to Jay.");
+      $('#option2').html("Play with Blue Ivy.");
+    } else if(count == 5){
+      $('.textbox').html("Jay doesn't agree with your actions, but he won't judge you for your choice.");
+      $('#option1').html("Should you come clean?");
+      $('#option2').html("Keep running away, Jay won't stand for this, and he won't offer you refuge.");
+    } else if (count ==6){
+      $('.textbox').html("Didn't you learn the first time? Talking to Kim won't solve anything!");
+      $('#option1').html("Really now...");
+      $('#option2').html("Moral of the story is keep running from your problems.");
+    } else if(count ==7){
+      $('.textbox').html("Wow! A musical genie is waiting for you at your studio. He introduces himself as DJ Khaled. He's mad that you haven't hit him up for a song in a while. You have to answer his questions properly in order to pass.");
+      $('#option1').html("Are you ready to answer?");
+      $('#option2').html("Does it really matter if you are?")
+    } else if(count ==8){
+      $('.textbox').html("Generic question here.");
+      $('#option1').html("Generic answer 1"); //this one is death
+      $('#option2').html("Generic answer 2"); //this one is life
     }
   });
 
@@ -45,14 +65,30 @@ $(document).ready(function(){
       $('.textbox').html('You really want to talk to Kim after you cheated? Brave man.');
       $('#option1').html('Stick with programming.');
       $('#option2').html("You don't need to be in the field of saving people.");
+    } else if( count ==3){
+      $('.textbox').html("Cool, you made it to Jay's house. What do you want to do?");
+      $('#option1').html("Talk to Jay.");
+      $('#option2').html("Play with Blue Ivy.");
+    } else if( count ==4){
+      $('.textbox').html("Freak accident...Blue Ivy choked Kanye to death with a slinky.");
+      $('#option1').html("Why didn't Kanye fight back?");
+      $('#option2').html("He's a pacifist.");
+    } else if(count == 5){
+      $('.textbox').html("Jay doesn't agree with your actions, but he won't judge you for your choice.");
+      $('#option1').html("Should you come clean?");
+      $('#option2').html("Keep running away, Jay won't stand for this, and he won't offer you refuge.");
+    } else if(count ==6){
+      $('.textbox').html("Jay wants to know, where are you going to go?");
+      $('#option1').html("Go to the studio, music's always been your savior.");
+      $('#option2').html("There's a indie film showing that you've been wanting to see, It's Runaway, directed by you.")
+    } else if(count ==7){
+      $('.textbox').html("There's one other person in the theater. He's a world renowed Illuminati hunter, supposedly hunting Jay-z. But I guess you'll have to do.");
+      $('#option1').html("You're a real stand-up guy, Kanye.");
+      $('#option2').html("I heard the movie got some pretty good reviews though.");
     }
   });
 
-  $('#option1').click(function(){
+
 
   })
 
-
-
-
-})
